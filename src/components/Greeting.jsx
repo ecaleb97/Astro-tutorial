af50501d5f4ca9ef04577ff2f93acf112d7d30dc
ecaleb97 <valley_rob.0c@icloud.com>
2023-12-07 11:13:07 +0100

    Add React JSX support and update dependencies

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+export default function Greeting({ messages }) {
+  const randomMessage = messages[(Math.floor(Math.random() * messages.length))]
+
+  const [greeting, setGreeting] = useState(messages[0])
+
+  const handleClick = () => {
+    setGreeting(randomMessage)
+  }
+
+  return (
+    <div>
+      <h3>{greeting}! Thank you for visiting!</h3>
+      <button onClick={handleClick}>New Greeting</button>
+    </div>
+  )
+}
